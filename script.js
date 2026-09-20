67d01544e22704e60aa1501f104d0ceec3fb8efe
Mohammad Foysal <ukdlw.ie1@gmail.com>
2026-09-20 18:04:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Toggle Navigation Menu Icon for Mobile View
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+// Highlight Active Link On Scroll
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Close Mobile Menu on Click/Scroll
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
